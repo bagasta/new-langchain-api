221702c107a2119180a4021cc2af518c875d5bd1
bagas bgs <bagasbgs2516@gmail.com>
2026-01-30 19:10:41 +0700

    new

--- fix_alembic_migration.sh
+++ fix_alembic_migration.sh
@@ -16,7 +16,7 @@ NC='\033[0m' # No Color
 
 # 1. Check git commit
 echo -e "${YELLOW}Step 1: Checking git status...${NC}"
-cd /home/bagas/Langchain-API-new
+cd "$(dirname "$0")"
 CURRENT_COMMIT=$(git log --oneline -1 | awk '{print $1}')
 echo "Current commit: $CURRENT_COMMIT"
 
